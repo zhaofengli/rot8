c966bc130956dd3a9acf4368ba819995c8bee128
Zhaofeng Li <hello@zhaofeng.li>
2020-09-11 21:03:24 -0700

    My neck no longer hurts :)
    This commit adds a bunch of features, including:
    
    * Use the correct accelerometer value scale
    
    Not all devices have the accelerometer raw values between 0 and 1 (e.g. PineTab).
    The correct scale is accessible via sysfs.
    
    * Revamped keyboard mode system
    
    You can specify how your keyboard is attached so that rot8 will do the
    appropriate thing. `--keyboard-mode` can be one of:
    
    - `integrated`: The keyboard is an integral part of the device. We will
      disable the keyboard when the screen is not oriented upright.
    - `detachable`: The keyboard is detachable. We will keep the screen
      oriented upright when the keyboard is attached.
    - `none`: We won't touch your keyboard.
    
    The old `-k`/`--disable-keyboard` flag has been removed.
    
    * After-rotation hook
    
    Specify a command to be run in shell after rotation with `--rotate-hook`. Useful
    for forcing a restart of Polybar, for example.
    
    * PineTab-specific flags
    
    There are several PineTab-specific additions. If you are using a
    PineTab, the following flags should be given:
    
    --display DSI-1 --touchscreen 'pointer:Goodix Capacitive TouchScreen' --flip-y
    --rotate-90 --keyboard-mode detachable --keyboard 'HAILUCK CO.,LTD USB KEYBOARD'
    
    If you are setting the default rotation in Xorg.conf, use `TransformationMatrix`
    instead of `CalibrationMatrix` for the touchscreen, otherwise the setting will
    conflict with rot8.
    
    Issue: efernau/rot8#15

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ extern crate glob;
 extern crate regex;
 
 use std::fs;
-use std::process::Command;
+use std::process::{Command, Stdio};
 use std::thread;
 use std::time::Duration;
 
@@ -15,6 +15,12 @@ use serde_json::Value;
 enum Backend {
     Sway,
     Xorg,
+}
+
+enum KeyboardMode {
+    Integrated,
+    Detachable,
+    None,
 }
 
 #[derive(Deserialize)]
@@ -51,6 +57,31 @@ fn get_keyboards(backend: &Backend) -> Result<Vec<String>, String> {
         }
         Backend::Xorg => {
             return Ok(vec![]);
+        }
+    }
+}
+
+fn keyboards_attached<T: AsRef<std::ffi::OsStr>>(backend: &Backend, keyboards: &[T]) -> bool {
+    match backend {
+        Backend::Sway => {
+            // TODO
+            false
+        }
+        Backend::Xorg => {
+            for keyboard in keyboards {
+                let probe = Command::new("xinput")
+                    .arg("list")
+                    .arg(&keyboard)
+                    .stdout(Stdio::null())
+                    .stderr(Stdio::null())
+                    .status()
+                    .expect("Xinput list command failed to start");
+
+                if probe.success() {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
@@ -117,6 +148,19 @@ fn get_window_server_rotation_state(display: &str, backend: &Backend) -> Result<
     }
 }
 
+fn get_scale() -> Option<f32> {
+    match glob("/sys/bus/iio/devices/iio:device*/in_accel_scale") {
+        Ok(mut paths) => {
+            let path = paths.next()?.ok()?;
+            let scale_raw = fs::read_to_string(path).ok()?;
+            let scale = scale_raw.trim_end_matches('\n').parse::<f32>().ok()?;
+            Some(scale)
+        }
+        Err(_) => None,
+    }
+}
+
+#[derive(Debug)]
 struct Orientation {
     vector: (f32, f32),
     new_state: &'static str,
@@ -126,10 +170,11 @@ struct Orientation {
 
 fn main() -> Result<(), String> {
     let mut new_state: &str;
+    let mut x_state: &str;
+
     let mut path_x: String = "".to_string();
     let mut path_y: String = "".to_string();
     let mut matrix: [&str; 9];
-    let mut x_state: &str;
 
     let backend = if String::from_utf8(Command::new("pidof").arg("sway").output().unwrap().stdout)
         .unwrap()
@@ -147,7 +192,7 @@ fn main() -> Result<(), String> {
         return Err("Unable to find Sway or Xorg procceses".to_owned());
     };
 
-    let mut args = vec![
+    let args = vec![
         Arg::with_name("sleep")
             .default_value("500")
             .long("sleep")
@@ -176,20 +221,41 @@ fn main() -> Result<(), String> {
             .value_name("THRESHOLD")
             .help("Set a rotation threshold between 0 and 1")
             .takes_value(true),
-    ];
 
-    match backend {
-        Backend::Sway => {
-            args.push(
-                Arg::with_name("keyboard")
-                    .long("disable-keyboard")
-                    .short("k")
-                    .help("Disable keyboard for tablet modes (Sway only)")
-                    .takes_value(false),
-            );
-        }
-        Backend::Xorg => { /* Keyboard disabling in Xorg is not supported yet */ }
-    }
+        Arg::with_name("keyboard_mode")
+            .default_value("integrated")
+            .long("keyboard-mode")
+            .value_name("KEYBOARD_MODE")
+            .help(
+                "'integrated' - The keyboard is an integral part of the device. Disable it when device is rotated (Sway only).\n\
+                'detachable' - The keyboard is detachable. Lock the rotation when it's attached.\n\
+                'none' - Do not enable/disable keyboard"
+            )
+            .takes_value(true),
+        Arg::with_name("keyboard_device")
+            .long("keyboard")
+            .value_name("KEYBOARD_DEVICE")
+            .help("Set keyboard device")
+            .takes_value(true),
+
+        // PineTab Hack
+        Arg::with_name("rotate_90")
+            .long("rotate-90")
+            .value_name("ROTATE_90")
+            .help("[PineTab Hack] Enable if the content is 90 degrees counterclockwise when upright")
+            .takes_value(false),
+        Arg::with_name("flip_y")
+            .long("flip-y")
+            .value_name("FLIP_Y")
+            .help("[PineTab Hack] Flip Y axis")
+            .takes_value(false),
+
+        Arg::with_name("rotate_hook")
+            .long("rotate-hook")
+            .value_name("ROTATE_HOOK")
+            .help("A shell command to run after rotation")
+            .takes_value(true),
+    ];
 
     let cmd_lines = App::new("rot8").version("0.1.3").args(&args);
 
@@ -198,12 +264,30 @@ fn main() -> Result<(), String> {
     let sleep = matches.value_of("sleep").unwrap_or("default.conf");
     let display = matches.value_of("display").unwrap_or("default.conf");
     let touchscreen = matches.value_of("touchscreen").unwrap_or("default.conf");
-    let disable_keyboard = matches.is_present("keyboard");
     let threshold = matches.value_of("threshold").unwrap_or("default.conf");
     let old_state_owned = get_window_server_rotation_state(display, &backend)?;
     let mut old_state = old_state_owned.as_str();
 
-    let keyboards = get_keyboards(&backend)?;
+    let keyboard_mode = match matches.value_of("keyboard_mode") {
+        Some("integrated") => KeyboardMode::Integrated,
+        Some("detachable") => KeyboardMode::Detachable,
+        Some("none") => KeyboardMode::None,
+        _ => panic!("--keyboard-mode can be one of 'integrated', 'detachable', and 'none'"),
+    };
+
+    let keyboards = if matches.is_present("keyboard_device") {
+        vec![String::from(matches.value_of("keyboard_device").unwrap())]
+    } else {
+        get_keyboards(&backend)?
+    };
+
+    // PineTab Hack
+    let rotate_90 = matches.is_present("rotate_90");
+    let flip_y = matches.is_present("flip_y");
+
+    let rotate_hook = matches.value_of("rotate_hook");
+
+    let scale = get_scale();
 
     for entry in glob("/sys/bus/iio/devices/iio:device*/in_accel_*_raw").unwrap() {
         match entry {
@@ -254,15 +338,39 @@ fn main() -> Result<(), String> {
     loop {
         let x_raw = fs::read_to_string(path_x.as_str()).unwrap();
         let y_raw = fs::read_to_string(path_y.as_str()).unwrap();
-        let x_clean = x_raw.trim_end_matches('\n').parse::<i32>().unwrap_or(0);
-        let y_clean = y_raw.trim_end_matches('\n').parse::<i32>().unwrap_or(0);
+        let x_clean: f32 = x_raw.trim_end_matches('\n').parse::<i32>().unwrap_or(0) as f32;
+        let mut y_clean: f32 = y_raw.trim_end_matches('\n').parse::<i32>().unwrap_or(0) as f32;
+
+        let human_normal = if rotate_90 {
+            "90"
+        } else {
+            "normal"
+        };
+
+        if flip_y {
+            y_clean = -y_clean;
+        }
 
         // Normalize vectors
-        let x: f32 = (x_clean as f32) / 1e6;
-        let y: f32 = (y_clean as f32) / 1e6;
+        let (mut x, mut y): (f32, f32) = match scale {
+            Some(scale) => {
+                (x_clean * scale / 10f32, y_clean * scale / 10f32)
+            }
+            None => (x_clean / 1f32, y_clean / 1f32),
+        };
+
+
+        // Rotate (HACK)
+        if rotate_90 {
+            // Rotate 90deg clockwise
+            let mx = -x;
+            x = y;
+            y = mx;
+        }
 
         for (_i, orient) in orientations.iter().enumerate() {
             let d = (x - orient.vector.0).powf(2.0) + (y - orient.vector.1).powf(2.0);
+
             if d < threshold.parse::<f32>().unwrap_or(0.5) {
                 current_orient = &orient;
                 break;
@@ -274,55 +382,81 @@ fn main() -> Result<(), String> {
         matrix = current_orient.matrix;
 
         if new_state != old_state {
-            let keyboard_state = if new_state == "normal" {
+            let integrated_keyboard_state = if new_state == human_normal {
                 "enabled"
             } else {
                 "disabled"
             };
-            match backend {
-                Backend::Sway => {
-                    Command::new("swaymsg")
-                        .arg("output")
-                        .arg(display)
-                        .arg("transform")
-                        .arg(new_state)
-                        .spawn()
-                        .expect("Swaymsg rotate command failed to start")
-                        .wait()
-                        .expect("Swaymsg rotate command wait failed");
-                    if disable_keyboard {
-                        for keyboard in &keyboards {
-                            //                            println!("swaymsg input {} events {}", keyboard, keyboard_state);
-                            Command::new("swaymsg")
-                                .arg("input")
-                                .arg(keyboard)
-                                .arg("events")
-                                .arg(keyboard_state)
-                                .spawn()
-                                .expect("Swaymsg keyboard command failed to start")
-                                .wait()
-                                .expect("Swaymsg keyboard command wait failed");
+
+            println!("{} -> {} (human_normal is {})", old_state, new_state, human_normal);
+            let noop = if let KeyboardMode::Detachable = keyboard_mode {
+                // If there are keyboards attached, refuse to rotate to
+                // any orientation but human_normal
+                keyboards_attached(&backend, &keyboards) &&
+                (old_state == human_normal || new_state != human_normal)
+            } else {
+                false
+            };
+
+            if !noop {
+                match backend {
+                    Backend::Sway => {
+                        Command::new("swaymsg")
+                            .arg("output")
+                            .arg(display)
+                            .arg("transform")
+                            .arg(new_state)
+                            .spawn()
+                            .expect("Swaymsg rotate command failed to start")
+                            .wait()
+                            .expect("Swaymsg rotate command wait failed");
+
+                        if let KeyboardMode::Integrated = keyboard_mode {
+                            // Disable integrated keyboard when not human_normal
+                            for keyboard in &keyboards {
+                                Command::new("swaymsg")
+                                    .arg("input")
+                                    .arg(keyboard)
+                                    .arg("events")
+                                    .arg(integrated_keyboard_state)
+                                    .spawn()
+                                    .expect("Swaymsg keyboard command failed to start")
+                                    .wait()
+                                    .expect("Swaymsg keyboard command wait failed");
+                            }
                         }
                     }
-                }
-                Backend::Xorg => {
-                    Command::new("xrandr")
-                        .arg("-o")
-                        .arg(x_state)
-                        .spawn()
-                        .expect("Xrandr rotate command failed to start")
-                        .wait()
-                        .expect("Xrandr rotate command wait failed");
+                    Backend::Xorg => {
+                        Command::new("xrandr")
+                            .arg("--output")
+                            .arg(display)
+                            .arg("--rotate")
+                            .arg(x_state)
+                            .spawn()
+                            .expect("Xrandr rotate command failed to start")
+                            .wait()
+                            .expect("Xrandr rotate command wait failed");
 
-                    Command::new("xinput")
-                        .arg("set-prop")
-                        .arg(touchscreen)
-                        .arg("Coordinate Transformation Matrix")
-                        .args(&matrix)
+                        Command::new("xinput")
+                            .arg("set-prop")
+                            .arg(touchscreen)
+                            .arg("Coordinate Transformation Matrix")
+                            .args(&matrix)
+                            .spawn()
+                            .expect("Xinput rotate command failed to start")
+                            .wait()
+                            .expect("Xinput rotate command wait failed");
+
+                    }
+                }
+                if let Some(hook) = rotate_hook {
+                    Command::new("/bin/sh")
+                        .arg("-c")
+                        .arg(hook)
                         .spawn()
-                        .expect("Xinput rotate command failed to start")
+                        .expect("Rotate hook command failed to start")
                         .wait()
-                        .expect("Xinput rotate command wait failed");
+                        .expect("Rotate hook command wait failed");
                 }
             }
             old_state = new_state;
